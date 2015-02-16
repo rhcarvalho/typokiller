224de239a217be7e896c05c5547a2b07eda52566
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-02-17 00:06:56 +0100

    Test Offset, Line and Column computation

--- read_test.go
+++ read_test.go
@@ -46,6 +46,24 @@ func TestReadDirAsciiDoc(t *testing.T) {
 	if got, want := len(pkg.Documentation), 6; got != want {
 		t.Errorf("len(pkg.Documentation) = %d, want %d", got, want)
 	}
+	for i, offsetLineColumn := range [][3]int{
+		{0, 1, 1},
+		{76, 5, 1},
+		{156, 8, 1},
+		{326, 13, 1},
+		{345, 15, 1},
+		{373, 19, 1},
+	} {
+		if got, want := pkg.Documentation[i].Position.Offset, offsetLineColumn[0]; got != want {
+			t.Errorf("pkg.Documentation[%d].Offset = %d, want %d", i, got, want)
+		}
+		if got, want := pkg.Documentation[i].Position.Line, offsetLineColumn[1]; got != want {
+			t.Errorf("pkg.Documentation[%d].Line = %d, want %d", i, got, want)
+		}
+		if got, want := pkg.Documentation[i].Position.Column, offsetLineColumn[2]; got != want {
+			t.Errorf("pkg.Documentation[%d].Column = %d, want %d", i, got, want)
+		}
+	}
 }
 
 func TestBadPath(t *testing.T) {
